5337a89910c29c83e9db0ab75ca615e8efefe736
TabataCorrea <tcoscorrea@hotmail.com>
2022-05-19 17:47:16 -0300

    Lista de exercícios

--- 21. aula 46 exceções JS/README.md
+++ 21. aula 46 exceções JS/README.md
@@ -1,3 +1,8 @@
 <h1 align="center">💻JavaScript - Exceções📚</h1>
 
 ## índice:
+- [Exemplo 1](exemplo01.js)
+- [Exemplo 2](exemplo02.js)
+- [Exemplo 3](exemplo03.js)
+- [Exemplo 4](exemplo04.js)
+- [Exemplo 5](exemplo06.html)
---
+++ 21. aula 46 exceções JS/exemplo03.js
@@ -0,0 +1,23 @@
+let x = 100
+//tenta executar uma função
+try {
+    
+    //se a condição é válida, então executa 
+    if(x < 30){
+        console.log("O número é válido.")
+    
+    //se a condição é inválida, lança um erro com mensagem personalizada
+    }else{
+        //erro do tipo objeto com atributos "name" e "message" personalizados
+        // throw {
+        //     "name":"ErroNumeroInvalido",
+        //     "message":"O número é inválido"
+        // }
+
+        throw "Número inválido"
+    }
+
+//captura o erro e trata
+} catch (error) {
+    console.log(error)
+}
